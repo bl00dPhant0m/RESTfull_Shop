4bff161d763ef7060e65db08bab349626539fa78
bl00dPhant0m <terehovks.n8@gmail.com>
2024-10-26 15:41:21 +0300

    Initial commit

--- src/main/java/org/spring/restfull_shop/repository/BasketRepository.java
+++ src/main/java/org/spring/restfull_shop/repository/BasketRepository.java
@@ -2,7 +2,6 @@ package org.spring.restfull_shop.repository;
 
 import org.spring.restfull_shop.entity.Basket;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
--- src/main/java/org/spring/restfull_shop/service/basket/BasketService.java
+++ src/main/java/org/spring/restfull_shop/service/basket/BasketService.java
@@ -4,7 +4,6 @@ package org.spring.restfull_shop.service.basket;
 import org.spring.restfull_shop.entity.Basket;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface BasketService {
     Basket addBasket(Basket basket);
